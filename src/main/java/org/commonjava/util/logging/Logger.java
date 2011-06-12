0f961049e2eef167e09d3a521ae38cd910c265f4
John Casey <jdcasey@commonjava.org>
2011-06-12 19:08:06 -0400

    setting slf4j version to 1.5.6

--- src/main/java/org/commonjava/util/logging/Logger.java
+++ src/main/java/org/commonjava/util/logging/Logger.java
@@ -16,160 +16,160 @@
 
 package org.commonjava.util.logging;
 
-import org.slf4j.LoggerFactory;
-
 import java.text.MessageFormat;
 import java.util.IllegalFormatException;
 
+import org.slf4j.LoggerFactory;
+
 public final class Logger
 {
-    
+
     private final org.slf4j.Logger logger;
-    
-    public Logger( Class<?> clazz )
+
+    public Logger( final Class<?> clazz )
     {
-        logger = LoggerFactory.getLogger( clazz );
+        logger = LoggerFactory.getLogger( clazz.getName() );
     }
-    
-    public Logger( String name )
+
+    public Logger( final String name )
     {
         logger = LoggerFactory.getLogger( name );
     }
-    
-    public Logger( org.slf4j.Logger logger )
+
+    public Logger( final org.slf4j.Logger logger )
     {
         this.logger = logger;
     }
-    
+
     public org.slf4j.Logger getLogger()
     {
         return logger;
     }
-    
-    public Logger debug( String format, Object...params )
+
+    public Logger debug( final String format, final Object...params )
     {
         if ( logger.isDebugEnabled() )
         {
             logger.debug( format( format, params ) );
         }
-        
+
         return this;
     }
 
-    public Logger debug( String format, Throwable error, Object...params )
+    public Logger debug( final String format, final Throwable error, final Object...params )
     {
         if ( logger.isDebugEnabled() )
         {
             logger.debug( format( format, params ), error );
         }
-        
+
         return this;
     }
 
-    public Logger error( String format, Object...params )
+    public Logger error( final String format, final Object...params )
     {
         if ( logger.isErrorEnabled() )
         {
             logger.error( format( format, params ) );
         }
-        
+
         return this;
     }
 
-    public Logger error( String format, Throwable error, Object...params )
+    public Logger error( final String format, final Throwable error, final Object...params )
     {
         if ( logger.isErrorEnabled() )
         {
             logger.error( format( format, params ), error );
         }
-        
+
         return this;
     }
 
-    public Logger info( String format, Object...params )
+    public Logger info( final String format, final Object...params )
     {
         if ( logger.isInfoEnabled() )
         {
             logger.info( format( format, params ) );
         }
-        
+
         return this;
     }
 
-    public Logger info( String format, Throwable error, Object...params )
+    public Logger info( final String format, final Throwable error, final Object...params )
     {
         if ( logger.isInfoEnabled() )
         {
             logger.info( format( format, params ), error );
         }
-        
+
         return this;
     }
 
-    public Logger trace( String format, Object...params )
+    public Logger trace( final String format, final Object...params )
     {
         if ( logger.isTraceEnabled() )
         {
             logger.trace( format( format, params ) );
         }
-        
+
         return this;
     }
 
-    public Logger trace( String format, Throwable error, Object...params )
+    public Logger trace( final String format, final Throwable error, final Object...params )
     {
         if ( logger.isTraceEnabled() )
         {
             logger.trace( format( format, params ), error );
         }
-        
+
         return this;
     }
 
-    public Logger warn( String format, Object...params )
+    public Logger warn( final String format, final Object...params )
     {
         if ( logger.isWarnEnabled() )
         {
             logger.warn( format( format, params ) );
         }
-        
+
         return this;
     }
 
-    public Logger warn( String format, Throwable error, Object...params )
+    public Logger warn( final String format, final Throwable error, final Object...params )
     {
         if ( logger.isWarnEnabled() )
         {
             logger.warn( format( format, params ), error );
         }
-        
+
         return this;
     }
 
-    private String format( String format, Object[] params )
+    private String format( final String format, final Object[] params )
     {
         String out = format;
         if ( params == null || params.length < 1 )
         {
             return out;
         }
-        
+
         try
         {
             out = String.format( out, params );
         }
-        catch ( IllegalFormatException e )
+        catch ( final IllegalFormatException e )
         {
             try
             {
                 out = MessageFormat.format( out, params );
             }
-            catch ( IllegalArgumentException e1 )
+            catch ( final IllegalArgumentException e1 )
             {
                 out = format;
             }
         }
-        
+
         return out;
     }
 
